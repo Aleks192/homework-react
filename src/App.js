66e798089607e725567346b335e030356d2b7559
Aleks192 <alexandru.ovidiu.ghita192@gmail.com>
2022-06-05 20:53:44 +0300

    New features :D

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://media-exp2.licdn.com/dms/image/C4D03AQHP4OIhyaNsSw/profile-displayphoto-shrink_800_800/0/1583447287301?e=1660176000&v=beta&t=YUzEwLH4IzkAnv7d_02afiAjGu7yYxfA3CVMcSr_G5c" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Salutare! <code>Eu sunt Alexandru Ghita</code> Bun venit in aplicatia mea de React.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pentru a invata React te rog sa dai click pe mine!
         </a>
       </header>
     </div>
